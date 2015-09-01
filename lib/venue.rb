3d0a75e96bd1bd4de6f70958b3b7f78c0a8a0fe4
Kendra Ash <kendra.ash32@gmail.com>
2015-08-31 21:28:25 -0700

    made changes to methods in Venue class

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,16 +1,11 @@
 class Venue < ActiveRecord::Base
   has_and_belongs_to_many(:bands)
   validates(:name, :presence => true)
-  before_save(:uppercase_first_letter, :adding_time_event_date)
+  before_save(:uppercase_first_letter)
 
   private
 
     define_method(:uppercase_first_letter) do
       self.name = name.downcase!().split.map(&:capitalize).join(' ')
-    end
-    define_method(:adding_time_event_date) do
-      if(self.event_date != nil)
-        self.event_date = event_date.(concat('00:00:00'))
-      end
     end
 end
