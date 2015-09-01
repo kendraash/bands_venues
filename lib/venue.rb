8fd41c652127a8a7ebee66ccaa600128ff8b1174
Kendra Ash <kendra.ash32@gmail.com>
2015-08-31 21:06:43 -0700

    fixing bug in concatenating time to event_date

--- lib/venue.rb
+++ lib/venue.rb
@@ -9,16 +9,8 @@ class Venue < ActiveRecord::Base
       self.name = name.downcase!().split.map(&:capitalize).join(' ')
     end
     define_method(:adding_time_event_date) do
-      self.event_date = event_date.(concat('00:00:00'))
-    end
-
-    define_singleton_method(:find_band_event_date) do |venue, band_name|
-      filtered_arr = []
-      venue.bands do |band|
-        if band_name.eql?(band.name)
-          filtered_arr << venue.event_date
-        end
+      if(self.event_date != nil)
+        self.event_date = event_date.(concat('00:00:00'))
       end
     end
-  filtered_arr
 end
